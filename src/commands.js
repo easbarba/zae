ccd20efe2df06f9e7c98fb1d94e6a310cb1edc72
easbarba <easbarba@protonmail.com>
2022-01-08 02:15:01 -0300

    add Commands placeholder methods

---
+++ src/commands.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Commands {
+  clean() { return 'clean' }
+  download() { return 'download' }
+  info() { return 'info' }
+  install() { return 'install' }
+  installed() { return 'installed' }
+  remove() { return 'remove' }
+  search() { return 'search' }
+  update() { return 'update' }
+  upgrade() { return 'upgrade' }
+}
