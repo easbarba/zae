5ee045f9779dee1151305b2095bdf245f56444ff
easbarba <easbarba@protonmail.com>
2022-01-14 18:42:41 -0300

    prettier: no semi for me too

--- bin/distro.ts
+++ bin/distro.ts
@@ -1,95 +1,95 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from 'commander'
 
-import Index from '../src/index.ts';
+import Index from '../src/index.ts'
 
-const program = new Command();
+const program = new Command()
 
-const index = new Index('install');
-
-program
-  .command('clean')
-  .description('clean system residual packages dependencies')
-  .action(() => {
-    console.log('cleaned');
-  });
+const index = new Index('install')
 
 program
-  .command('download')
-  .description('download the given binary package into the current directory')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('clean')
+    .description('clean system residual packages dependencies')
+    .action(() => {
+        console.log('cleaned')
+    })
 
 program
-  .command('fix')
-  .description('fix system issues')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('download')
+    .description('download the given binary package into the current directory')
+    .action(() => {
+        console.log('downloaded')
+    })
 
 program
-  .command('info')
-  .description('view info about a specific package')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('fix')
+    .description('fix system issues')
+    .action(() => {
+        console.log('downloaded')
+    })
 
 program
-  .command('install')
-  .description('Install a Package from Repositories')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('info')
+    .description('view info about a specific package')
+    .action(() => {
+        console.log('downloaded')
+    })
 
 program
-  .command('installed')
-  .description('list installed packages')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('install')
+    .description('Install a Package from Repositories')
+    .action(() => {
+        console.log('downloaded')
+    })
 
 program
-  .command('remove')
-  .description('remove one or more installed packages')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('installed')
+    .description('list installed packages')
+    .action(() => {
+        console.log('downloaded')
+    })
 
-program.command('search <package>').description('find a package package');
+program
+    .command('remove')
+    .description('remove one or more installed packages')
+    .action(() => {
+        console.log('downloaded')
+    })
+
+program.command('search <package>').description('find a package package')
 // .action((package) => {
 //   console.log(package)
 // })
 
 program
-  .command('update')
-  .description('update package lists')
-  .action(() => {
-    console.log('downloaded');
-  });
+    .command('update')
+    .description('update package lists')
+    .action(() => {
+        console.log('downloaded')
+    })
 
 program
-  .command('upgrade')
-  .description('upgrade installed packages')
-  .action(() => {
-    index.action = 'upgrade';
-    console.log(`command: ${index.run()}`);
-  });
+    .command('upgrade')
+    .description('upgrade installed packages')
+    .action(() => {
+        index.action = 'upgrade'
+        console.log(`command: ${index.run()}`)
+    })
 
 program
-  .command('system')
-  .description('list system information')
-  .action(() => {
-    // console.log(`manager: ${p.executable()}`)
-    // console.log(`location: ${p.location()}`)
-  });
+    .command('system')
+    .description('list system information')
+    .action(() => {
+        // console.log(`manager: ${p.executable()}`)
+        // console.log(`location: ${p.location()}`)
+    })
 
 program
-  .command('version')
-  .description('package version')
-  .action(() => {
-    console.log('version: 0.0.1');
-  });
+    .command('version')
+    .description('package version')
+    .action(() => {
+        console.log('version: 0.0.1')
+    })
 
-program.parse(process.argv);
+program.parse(process.argv)
