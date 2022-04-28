700a29f401e2d737c011d042e1227669296d7da0
easbarba <easbarba@outlook.com>
2022-04-28 04:10:30 -0300

    need: disable for now

--- lib/pak/need.rb
+++ lib/pak/need.rb
@@ -1,11 +1,15 @@
 module Pak
   # define how command should be run, either to become super user or not.
-  class Need
+  class Become
+    def initialize(packager)
+      @packager = packager
+    end
+
     # commands
-    BECOME = @packager[:become].keys.dup.freez
+    # BECOME = @packager[:become].keys.dup.freeze
 
     # non-become commands
-    USER = @packager[:user].keys.dup.freeze
+    # USER = @packager[:user].keys.dup.freeze
 
     # query which become methods are available on user system
     def becomer; end
