4391a99f7ff01e1e7f6eaa3610dcb3d058bd9412
easbarba <easbarba@outlook.com>
2022-05-08 01:35:18 -0300

    lib/pak: translate_action is no more

--- lib/pak.rb
+++ lib/pak.rb
@@ -2,7 +2,6 @@ require_relative 'pak/commands'
 require_relative 'pak/config'
 require_relative 'pak/main'
 require_relative 'pak/need'
-require_relative 'pak/translate_action'
 require_relative 'pak/version'
 
 # a handy runner of distributions packages manager commands.
