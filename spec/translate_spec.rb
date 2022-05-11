b869c5aea9a2295705ae733b13537f008a39872f
easbarba <easbarba@outlook.com>
2022-05-11 12:49:30 -0300

    spec: add shallow translate spec

---
+++ spec/translate_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+require 'pak'
+
+module Pak
+  RSpec.describe Translate do
+    # context '#need' do
+    # it 'has become ones' do
+    #   commands = Pak::Commands.new(Config.new.found)
+
+    #   expect(commands.packager[:become]).to eq(raw_commands[:become])
+    # end
+
+    # it 'has user ones' do
+    #   commands = Pak::Commands.new(Config.new.found)
+    #   expect(commands.packager[:user]).to eq(raw_commands[:user])
+    # end
+
+    # it 'needs to be admin' do
+    #   need = Pak::Need.new(:install)
+    #   expect(need.admin?).to eq(true)
+    # end
+
+    # it 'needs arguments' do
+    #   need = Pak::Need.new(:install)
+    #   expect(need.arguments?).to eq(true)
+    # end
+
+    # it 'accepts whatever you wish, arguments or no' do
+    #   need = Pak::Need.new(:autoremove)
+    #   expect(need.whatever?).to eq(true)
+    # end
+    # end
+  end
+end
