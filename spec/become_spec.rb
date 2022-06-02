211ecf8683b695a22028df0e78536be5b32161da
easbarba <easbarba@outlook.com>
2022-06-02 19:22:08 -0300

    spec: add become examples

--- spec/become_spec.rb
+++ spec/become_spec.rb
@@ -6,5 +6,24 @@ require 'zae'
 
 module Zae
   RSpec.describe Become do
+    let(:config) do
+      {
+        exec: '/usr/bin/binx',
+        become: { remove: 'uninstall' },
+        user: { search: 'query -f' }
+      }
+    end
+
+    context '#need?' do
+      it 'run command as superuser' do
+        become = Become.new :remove, config
+        expect(become.need?).to eq true
+      end
+
+      it 'run command as user' do
+        become = Become.new :search, config
+        expect(become.need?).to eq false
+      end
+    end
   end
 end
